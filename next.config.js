3ddf057d035e3a487006a0c38d122bd5a31275d2
1205762664@qq.com <1205762664@qq.com>
2025-12-08 22:10:54 +0800

    vercel修改11

--- next.config.js
+++ next.config.js
@@ -2,16 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // ❌ 不要使用 export
-  // output: "export",
-
-  // ❌ 不需要 unoptimized，Vercel 有 Image Optimization
-  // images: { unoptimized: true },
-
-  // ❌ 不要 trailingSlash，否则 Vercel 会出现路由文件对不上
-  // trailingSlash: true,
-
-  // 你可以保留这个：用于自定义忽略文件，但不影响部署
+  
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
   experimental: {
     outputFileTracingExcludes: {
       '*': [
